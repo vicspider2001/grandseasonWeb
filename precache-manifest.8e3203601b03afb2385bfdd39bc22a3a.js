c51622917e74d38f1e39130288dc50c85a1663af
Victor Anogwih <anogwihvictor@gmail.com>
2023-12-26 13:25:44 -0500

    updatED

---
+++ precache-manifest.8e3203601b03afb2385bfdd39bc22a3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "150c0a854eeed448bf44c46c01f53741",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cad8124b30466a92a45a",
+    "url": "/static/css/2.7ae7b8b0.chunk.css"
+  },
+  {
+    "revision": "046e3e17fb30f18bbd60",
+    "url": "/static/css/main.59e98df8.chunk.css"
+  },
+  {
+    "revision": "cad8124b30466a92a45a",
+    "url": "/static/js/2.1fe66383.chunk.js"
+  },
+  {
+    "revision": "046e3e17fb30f18bbd60",
+    "url": "/static/js/main.a8e1f401.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
